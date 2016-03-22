6b31c14e8219d0e7f54e87a8ba777527fbdde965
Jon Jones <jon.p.jones13@gmail.com>
2016-03-22 13:56:06 -0500

    Added button drop down on left hand side. Created buttons programmatically, then applied gravity and collision behaviors to them.

---
+++ SlideOutTest/MenuLabel.swift
@@ -0,0 +1,14 @@
+//
+//  MenuLabel.swift
+//  SlideOutTest
+//
+//  Created by Jonathan Jones on 3/22/16.
+//  Copyright © 2016 JJones. All rights reserved.
+//
+
+import UIKit
+
+class MenuLabel: UILabel {
+    var segueID = ""
+    
+}
--- SlideOutTest/ViewController.swift
+++ SlideOutTest/ViewController.swift
@@ -10,12 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var buttonOne: UIButton!
+    var buttonDictionary = Dictionary<UIButton, String>()
+    var animator = UIDynamicAnimator()
+    var sidebarArray = [MenuLabel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        animator = UIDynamicAnimator.init(referenceView: self.view)
         buttonOne.backgroundColor = UIColor.lightGrayColor()
         buttonOne.setTitle("Tap Me!", forState: UIControlState.Normal)
         buttonOne.layer.cornerRadius = buttonOne.frame.width / 2
+        
+        
+// However many options that we want in our slide out menu, we can create that many buttons.
+//        
+//        setUpMenuItem("Main Page", segueID: "nil", position: 1)
+//        setUpMenuItem("First Page", segueID: "FirstSegueID", position: 2)
+//        setUpMenuItem("Second Page", segueID: "SecondSegueID", position: 3)
+//        
+        
+        
+    }
+    
+    func setUpMenuItem (buttonTitle: String, segueID: String, position: CGFloat) -> (MenuLabel) {
+        var sidebarLabel = MenuLabel()
+        sidebarLabel.text = buttonTitle
+        sidebarLabel.textColor = UIColor.whiteColor()
+        sidebarLabel.adjustsFontSizeToFitWidth = true
+        sidebarLabel.userInteractionEnabled = true
+        
+        sidebarLabel.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, 0.3 * self.view.frame.width, 0.1 * self.view.frame.height)
+        
+        self.view.addSubview(sidebarLabel)
+        self.view.bringSubviewToFront(sidebarLabel)
+        
+        let leftPoint = CGPointMake(view.frame.origin.x, position * 2 * sidebarLabel.frame.height)
+        
+        let rightPoint = CGPointMake(sidebarLabel.frame.width, position * 2 * sidebarLabel.frame.height)
+        
+        let gravity = UIGravityBehavior.init(items: [sidebarLabel])
+        let collision = UICollisionBehavior.init(items: [sidebarLabel])
+        
+        collision.addBoundaryWithIdentifier("bottom", fromPoint: leftPoint, toPoint: rightPoint)
+        
+        animator.addBehavior(gravity)
+        animator.addBehavior(collision)
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: "sidebarMenuLabelTapped:")
+        sidebarLabel.addGestureRecognizer(gestureRecognizer)
+        
+        sidebarArray.append(sidebarLabel)
+        return sidebarLabel
+    }
+    
+    func sidebarMenuLabelTapped (sender: UITapGestureRecognizer) {
+        print(sender)
         
     }
     
@@ -24,14 +73,29 @@ class ViewController: UIViewController {
     }
     
     func bounceOut () {
+        
+        
         let bounceViewImageView = setUpImageView()
         let bounceBGView = setUpBackgroundView()
         
         view.bringSubviewToFront(bounceViewImageView)
 
-        UIView.animateWithDuration(0.35) { () -> Void in
+//        UIView.animateWithDuration(0.35) { () -> Void in
+//            bounceViewImageView.frame = CGRectMake(0.3 * bounceViewImageView.frame.width, 0.15 * bounceViewImageView.frame.height, 0.8 * bounceViewImageView.frame.width, 0.8 * bounceViewImageView.frame.height)
+//            self.view.bringSubviewToFront(bounceViewImageView)
+//            
+//        }
+        
+        UIView.animateWithDuration(0.35, animations: { () -> Void in
+            
             bounceViewImageView.frame = CGRectMake(0.3 * bounceViewImageView.frame.width, 0.15 * bounceViewImageView.frame.height, 0.8 * bounceViewImageView.frame.width, 0.8 * bounceViewImageView.frame.height)
             self.view.bringSubviewToFront(bounceViewImageView)
+            
+            }) { (Bool) -> Void in
+                
+                self.setUpMenuItem("Main Page", segueID: "nil", position: 1)
+                self.setUpMenuItem("First Page", segueID: "FirstSegueID", position: 2)
+                self.setUpMenuItem("Second Page", segueID: "SecondSegueID", position: 3)
         }
     }
     
@@ -66,6 +130,7 @@ class ViewController: UIViewController {
         self.view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
         let bounceViewImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
+
         
         return bounceViewImage
     }
