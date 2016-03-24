3591aae03cd12c1ba1f38c36d7395e4cab55ea97
Jon Jones <jon.p.jones13@gmail.com>
2016-03-23 22:17:27 -0500

    Condensed all of the logic into a controller class. The 'returnToScreen' function is still behaving unexpectedly, but the segues are funcitonal.

--- SlideOutTest/SlideOutController.swift
+++ SlideOutTest/SlideOutController.swift
@@ -11,146 +11,163 @@ import UIKit
 
 class MenuLabel: UILabel {
     var segueID = ""
+    var position = CGFloat()
+    
 }
 
-private var sidebarArray = [MenuLabel]()
-private var menuDictionary = Dictionary<String,String>()
-
-func menuLabelTitles (menuItemDictionary: Dictionary<String,String>) {
-    menuDictionary = menuItemDictionary
-}
-
-
-// MARK: Setting Up Background and Image View
-
-private func setUpBackgroundView (soViewController: UIViewController) -> UIView {
-    let bgView = UIView()
-    bgView.frame = soViewController.view.frame
-    bgView.backgroundColor = UIColor.grayColor()
-    soViewController.view.addSubview(bgView)
-    soViewController.view.bringSubviewToFront(bgView)
+class SlideOutController: NSObject  {
     
-    return bgView
-}
-
-private func setUpImageView (soViewController: UIViewController) -> (UIImageView) {
-    let imageView = UIImageView()
-    imageView.frame = soViewController.view.frame
-    imageView.image = snapshotOfScreen(soViewController)
-    imageView.layer.cornerRadius = 15.0
-    imageView.clipsToBounds = true
-    soViewController.view.addSubview(imageView)
+    private var slideoutViewController = UIViewController()
+    private var sidebarArray = [MenuLabel]()
+    private var menuDictionary = Dictionary<String,[AnyObject]>()
     
-    imageView.userInteractionEnabled = true
-    
-    let gestureRecognizer = UITapGestureRecognizer(target: soViewController, action: Selector("returnToScreen"))
-    imageView.addGestureRecognizer(gestureRecognizer)
-    
-    return imageView
-}
-
-// MARK: Slide Overlayed Image View to Right, Slide labels into view
-
-private func bounceOut (soViewController: UIViewController) {
-    
-    let bounceViewImageView = setUpImageView(soViewController)
-    let bounceBGView = setUpBackgroundView(soViewController)
-    
-    var x = 1
-    for key in menuDictionary.keys {
-        let menuLabel = setUpMenuItem(soViewController, buttonTitle: key, segueID: menuDictionary[key]!, position: CGFloat(x))
-        sidebarArray.append(menuLabel)
-        x = x + 1
+    func slideOutWithMenu(soViewController: UIViewController, menuItemDictionary: Dictionary<String,[AnyObject]>) {
+        menuDictionary = menuItemDictionary
+        slideoutViewController = soViewController
+        bounceOut(soViewController)
+        
     }
     
-    soViewController.view.bringSubviewToFront(bounceViewImageView)
     
-    UIView.animateWithDuration(0.35, animations: { () -> Void in
+    // MARK: Setting Up Background and Image View
+    
+    private func setUpBackgroundView (soViewController: UIViewController) -> UIView {
+        let bgView = UIView()
+        bgView.frame = soViewController.view.frame
+        bgView.backgroundColor = UIColor.grayColor()
+        soViewController.view.addSubview(bgView)
+        soViewController.view.bringSubviewToFront(bgView)
         
-        bounceViewImageView.frame = CGRectMake(0.3 * bounceViewImageView.frame.width, 0.15 * bounceViewImageView.frame.height, 0.8 * bounceViewImageView.frame.width, 0.8 * bounceViewImageView.frame.height)
+        return bgView
+    }
+    
+    private func setUpImageView (soViewController: UIViewController) -> (UIImageView) {
+        let imageView = UIImageView()
+        imageView.frame = soViewController.view.frame
+        imageView.image = snapshotOfScreen(soViewController)
+        imageView.layer.cornerRadius = 15.0
+        imageView.clipsToBounds = true
+        soViewController.view.addSubview(imageView)
+        
+        imageView.userInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("returnToScreen:"))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        return imageView
+    }
+    
+    // MARK: Slide Overlayed Image View to Right, Slide labels into view
+    
+    private func bounceOut (soViewController: UIViewController) {
+        let bounceViewImageView = setUpImageView(soViewController)
+        setUpBackgroundView(soViewController)
+        
+        for key in menuDictionary.keys {
+            let labelName = key
+            let segueID = menuDictionary[key]![0] as! String
+            let position = CGFloat(menuDictionary[key]![1] as! CGFloat)
+            setUpMenuItem(soViewController, buttonTitle: labelName, segueID: segueID, position: position)
+        }
+        print("--> \(sidebarArray.count)")
         soViewController.view.bringSubviewToFront(bounceViewImageView)
         
-        }) { (Bool) -> Void in
+        UIView.animateWithDuration(0.35, animations: { () -> Void in
             
-            
-// SHOULD TAKE ARRAY OF DICTIONARIES MAYBE? KEY = LABEL TITLE AND VALUE = SEGUE ID
-            
-//            self.setUpMenuItem("Main Page", segueID: "", position: 1)
-//            self.setUpMenuItem("First Page", segueID: "FirstSegueID", position: 2)
-//            self.setUpMenuItem("Second Page", segueID: "SecondSegueID", position: 3)
-    }
-}
-
-
-
-private func snapshotOfScreen (soViewController: UIViewController) -> (UIImage) {
-    UIGraphicsBeginImageContext(soViewController.view.frame.size)
-    soViewController.view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
-    let bounceViewImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    
-    
-    return bounceViewImage
-}
-
-private func setUpMenuItem (soViewController: UIViewController, buttonTitle: String, segueID: String, position: CGFloat) -> (MenuLabel) {
-    var sidebarLabel = MenuLabel()
-    sidebarLabel.text = buttonTitle
-    sidebarLabel.textColor = UIColor.whiteColor()
-    sidebarLabel.adjustsFontSizeToFitWidth = true
-    sidebarLabel.userInteractionEnabled = true
-    
-    sidebarLabel.frame = CGRectMake(-(0.3 * soViewController.view.frame.width), 44 + 0.1 * position * soViewController.view.frame.height, 0.3 * soViewController.view.frame.width, 0.1 * soViewController.view.frame.height)
-    
-    soViewController.view.addSubview(sidebarLabel)
-    soViewController.view.bringSubviewToFront(sidebarLabel)
-    
-//    UIView.animateWithDuration(0.35, animations: { () -> Void in
-//        sidebarLabel.frame = CGRectMake(10, 44 + 0.1 * position * soViewController.view.frame.height, 0.3 * soViewController.view.frame.width, 0.1 * soViewController.view.frame.height)
-//        }) { (Bool) -> Void in
-//            
-//    }
-    
-    let gestureRecognizer = UITapGestureRecognizer(target: soViewController, action: "sidebarMenuLabelTapped:")
-    sidebarLabel.addGestureRecognizer(gestureRecognizer)
-    
-    sidebarArray.append(sidebarLabel)
-    print(sidebarArray.count)
-    return sidebarLabel
-}
-
-private func sidebarMenuLabelTapped (sender: UITapGestureRecognizer) {
-    print(sender)
-}
-
-
-private func returnToScreen (soViewController: UIViewController) {
-    let imageView = soViewController.view.subviews[soViewController.view.subviews.count - 1 - sidebarArray.count]
-    print(soViewController.view.subviews.count)
-    print(soViewController.view.subviews.count - 1 - sidebarArray.count)
-    
-    UIView.animateWithDuration(0.35, animations: { () -> Void in
-        
-        for label in sidebarArray {
-            label.center = CGPointMake(-label.frame.width, label.frame.midY)
-        }
-        
-        
-        imageView.frame = CGRectMake(soViewController.view.frame.origin.x, soViewController.view.frame.origin.y, soViewController.view.frame.width, soViewController.view.frame.height)
-        
-        
-        }) { (Bool) -> Void in
-            
-            imageView.removeFromSuperview()
-            for label in sidebarArray {
-                label.removeFromSuperview()
-                
+            for sidebarLabel in self.sidebarArray {
+                sidebarLabel.frame = CGRectMake(10, 44 + 0.1 * sidebarLabel.position * soViewController.view.frame.height, 0.3 * soViewController.view.frame.width - 20, 0.1 * soViewController.view.frame.height)
             }
-            sidebarArray.removeAll()
             
-            let bgView = soViewController.view.subviews.last
-            bgView?.removeFromSuperview()
+            bounceViewImageView.frame = CGRectMake(0.3 * bounceViewImageView.frame.width, 0.15 * bounceViewImageView.frame.height, 0.8 * bounceViewImageView.frame.width, 0.8 * bounceViewImageView.frame.height)
+            soViewController.view.bringSubviewToFront(bounceViewImageView)
             
+            }) { (Bool) -> Void in
+        }
+    }
+    
+    
+    
+    private func snapshotOfScreen (soViewController: UIViewController) -> (UIImage) {
+        UIGraphicsBeginImageContext(soViewController.view.frame.size)
+        soViewController.view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let bounceViewImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        
+        return bounceViewImage
+    }
+    
+    private func setUpMenuItem (soViewController: UIViewController, buttonTitle: String, segueID: String, position: CGFloat) -> (MenuLabel) {
+        let sidebarLabel = MenuLabel()
+        sidebarLabel.text = buttonTitle
+        sidebarLabel.textColor = UIColor.whiteColor()
+        sidebarLabel.adjustsFontSizeToFitWidth = true
+        sidebarLabel.userInteractionEnabled = true
+        
+        sidebarLabel.frame = CGRectMake(-(0.3 * soViewController.view.frame.width), 44 + 0.1 * position * soViewController.view.frame.height, 0.3 * soViewController.view.frame.width, 0.1 * soViewController.view.frame.height)
+        
+        sidebarLabel.segueID = segueID
+        sidebarLabel.position = position
+        
+        soViewController.view.addSubview(sidebarLabel)
+        soViewController.view.bringSubviewToFront(sidebarLabel)
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: "sidebarMenuLabelTapped:")
+        sidebarLabel.addGestureRecognizer(gestureRecognizer)
+        
+        sidebarArray.append(sidebarLabel)
+        return sidebarLabel
+    }
+    
+    func sidebarMenuLabelTapped(sender: UITapGestureRecognizer) {
+
+        let label = sender.view as! MenuLabel
+
+        if label.segueID == "" {
+            
+            returnToScreen(slideoutViewController)
+            
+        } else {
+            
+            slideoutViewController.performSegueWithIdentifier(label.segueID, sender: slideoutViewController)
+            
+        }
+    }
+    
+    
+    func returnToScreen (soViewController: UIViewController) {
+        print(sidebarArray.count)
+        print(slideoutViewController.view.subviews.count)
+        print(slideoutViewController.view.subviews.count - 1 - sidebarArray.count)
+        let imageView = slideoutViewController.view.subviews[slideoutViewController.view.subviews.count - 1 - sidebarArray.count]
+        print(imageView)
+        
+        
+        UIView.animateWithDuration(0.35, animations: { () -> Void in
+            
+            for label in self.sidebarArray {
+                label.center = CGPointMake(-label.frame.width, label.frame.midY)
+            }
+            
+            imageView.frame = CGRectMake(self.slideoutViewController.view.frame.origin.x, self.slideoutViewController.view.frame.origin.y, self.slideoutViewController.view.frame.width, self.slideoutViewController.view.frame.height)
+            
+            print(imageView.frame)
+            
+            }) { (Bool) -> Void in
+                
+                imageView.removeFromSuperview()
+                
+                for label in self.sidebarArray {
+                    label.removeFromSuperview()
+                }
+                
+                self.sidebarArray.removeAll()
+                self.menuDictionary.removeAll()
+                
+                let bgView = soViewController.view.subviews.last
+                bgView?.removeFromSuperview()
+                
+        }
     }
 }
 
--- SlideOutTest/ViewController.swift
+++ SlideOutTest/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var buttonOne: UIButton!
-    var buttonDictionary = Dictionary<UIButton, String>()
+    var menuDictionary = Dictionary<String, [AnyObject]>()
     var animator = UIDynamicAnimator()
-    var sidebarArray = [MenuLabel]()
+    let soc = SlideOutController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,127 +21,13 @@ class ViewController: UIViewController {
         buttonOne.setTitle("Tap Me!", forState: UIControlState.Normal)
         buttonOne.layer.cornerRadius = buttonOne.frame.width / 2
         
-    }
-    
-    func setUpMenuItem (buttonTitle: String, segueID: String, position: CGFloat) -> (MenuLabel) {
-        var sidebarLabel = MenuLabel()
-        sidebarLabel.text = buttonTitle
-        sidebarLabel.textColor = UIColor.whiteColor()
-        sidebarLabel.adjustsFontSizeToFitWidth = true
-        sidebarLabel.userInteractionEnabled = true
-        
-        sidebarLabel.frame = CGRectMake(-(0.3 * self.view.frame.width), 44 + 0.1 * position * self.view.frame.height, 0.3 * self.view.frame.width, 0.1 * self.view.frame.height)
-        
-        self.view.addSubview(sidebarLabel)
-        self.view.bringSubviewToFront(sidebarLabel)
-        
-        UIView.animateWithDuration(0.35, animations: { () -> Void in
-            sidebarLabel.frame = CGRectMake(10, 44 + 0.1 * position * self.view.frame.height, 0.3 * self.view.frame.width, 0.1 * self.view.frame.height)
-            }) { (Bool) -> Void in
-                
-        }
-        
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: "sidebarMenuLabelTapped:")
-        sidebarLabel.addGestureRecognizer(gestureRecognizer)
-        
-        sidebarArray.append(sidebarLabel)
-        print(sidebarArray.count)
-        return sidebarLabel
-    }
-    
-    func sidebarMenuLabelTapped (sender: UITapGestureRecognizer) {
-        print(sender)
-        
+        menuDictionary = ["Main Page": ["", 1],"First Page":["FirstSegueID", 2],"Second Page":["SecondSegueID", 3]]
+  
     }
     
     @IBAction func menuButtonTapped(sender: AnyObject) {
-        bounceOut()
+        soc.slideOutWithMenu(self, menuItemDictionary: menuDictionary)
     }
     
-    func bounceOut () {                                         ////////
-        
-        let bounceViewImageView = setUpImageView()
-        let bounceBGView = setUpBackgroundView()
-        
-        view.bringSubviewToFront(bounceViewImageView)
-        
-        UIView.animateWithDuration(0.35, animations: { () -> Void in
-            
-            bounceViewImageView.frame = CGRectMake(0.3 * bounceViewImageView.frame.width, 0.15 * bounceViewImageView.frame.height, 0.8 * bounceViewImageView.frame.width, 0.8 * bounceViewImageView.frame.height)
-            self.view.bringSubviewToFront(bounceViewImageView)
-            
-            }) { (Bool) -> Void in
-                
-                self.setUpMenuItem("Main Page", segueID: "", position: 1)
-                self.setUpMenuItem("First Page", segueID: "FirstSegueID", position: 2)
-                self.setUpMenuItem("Second Page", segueID: "SecondSegueID", position: 3)
-        }
-    }
-    
-    func setUpBackgroundView () -> UIView {                 ////////
-        let bgView = UIView()
-        bgView.frame = view.frame
-        bgView.backgroundColor = UIColor.grayColor()
-        view.addSubview(bgView)
-        view.bringSubviewToFront(bgView)
-        
-        return bgView
-    }
-    
-    func setUpImageView () -> (UIImageView) {               //////
-        let imageView = UIImageView()
-        imageView.frame = view.frame
-        imageView.image = snapshotOfScreen()
-        imageView.layer.cornerRadius = 15.0
-        imageView.clipsToBounds = true
-        view.addSubview(imageView)
-        
-        imageView.userInteractionEnabled = true
-        
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("returnToScreen"))
-        imageView.addGestureRecognizer(gestureRecognizer)
-        
-        return imageView
-    }
-    
-    func snapshotOfScreen () -> (UIImage) {                 //////
-        UIGraphicsBeginImageContext(self.view.frame.size)
-        self.view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
-        let bounceViewImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        
-        return bounceViewImage
-    }
-    
-    func returnToScreen () {                            //////
-         let imageView = self.view.subviews[self.view.subviews.count - 1 - self.sidebarArray.count]
-        print(self.view.subviews.count)
-        print(self.view.subviews.count - 1 - self.sidebarArray.count)
-        
-        UIView.animateWithDuration(0.35, animations: { () -> Void in
-            
-            for label in self.sidebarArray {
-                label.center = CGPointMake(-label.frame.width, label.frame.midY)
-            }
-            
-           
-            imageView.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.width, self.view.frame.height)
-
-            
-            }) { (Bool) -> Void in
-                
-                imageView.removeFromSuperview()
-                for label in self.sidebarArray {
-                    label.removeFromSuperview()
-                    
-                }
-                self.sidebarArray.removeAll()
-                
-                let bgView = self.view.subviews.last
-                bgView?.removeFromSuperview()
-
-        }
-    }
 }
 
